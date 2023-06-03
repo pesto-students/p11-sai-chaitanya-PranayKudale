8cedb49c7145e611272e24c914f5bbd828e4636a
PranayKudale <pranaykudale96@gmail.com>
2023-06-03 18:22:29 +0530

    Week 5 Assignments

---
+++ Week-5/Session 15/ES6Modules/index.js
@@ -0,0 +1,15 @@
+// index.js
+
+// Import the add function from calculator.js
+import { add } from "./calculator.js";
+
+// Use the add function to add two numbers and log the result
+const resultAdd = add(2, 3);
+console.log(resultAdd); // Output: 5
+
+// Import the subtract function from calculator.js
+import { subtract } from "./calculator.js";
+
+// Use the subtract function to subtract two numbers and log the result
+const resultSubtract = subtract(2, 3);
+console.log(resultSubtract); // Output: -1
